d8f77af8f717a29e51f3985440eb8d324748f82e
IvanDjuro <djurovic.ivan@outlook.fr>
2026-02-09 10:56:59 +0100

    cry finish

--- src/components/pokeCard/index.jsx
+++ src/components/pokeCard/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { getLocalizedName } from "../../utils/getLocalizedName.js";
 import HeaderCard from "../headerCard";
 import StatCard from "../statCard";
@@ -8,14 +8,38 @@ import "./index.css"
 
 const PokeCard = ({ pokemon, language }) => {
 
+    const audioRef = useRef(null);
+
     useEffect   (() => {
         console.log(pokemon);
 
     }, [pokemon]);
 
 
+    const handlerClick = (e) => {
+        if(e.shiftKey){
+            e.preventDefault(); 
+            e.stopPropagation(); 
+            crying();
+        }
+    }
+
+    const crying = async () => {
+        try {
+            if (!audioRef.current) {
+                audioRef.current = new Audio(pokemon.cry);
+                audioRef.current.preload = "auto";
+                audioRef.current.volume = 1;
+            } else {
+                audioRef.current.currentTime = 0;
+            }
+            await audioRef.current.play();
+        } catch (e) {
+            console.error("Impossible de jouer le son:", e);
+        }
+    }
     return (
-            <div className={`poke-card type-${pokemon.type[0].toLowerCase()}`}>
+            <div className={`poke-card type-${pokemon.type[0].toLowerCase()}`} onClick={handlerClick}>
                 <div className="poke-card-content">
 
                     <HeaderCard name={getLocalizedName(pokemon.name, language)} id={pokemon.id}/>
